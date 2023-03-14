dbd0b5591ce5a232b57b4ae1a20293f67c98db23
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-03-14 21:24:32 +0500

    add connectDB

--- server.js
+++ server.js
@@ -1,73 +1,74 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const path = require("path");
-const http = require("http");
+const path = require('path');
+const http = require('http');
 
-const express = require("express");
-const socketio = require("socket.io");
+const express = require('express');
+const socketio = require('socket.io');
 
-const formatMessage = require("./utils/messages");
+const connectDB = require('./db/connect');
+const formatMessage = require('./utils/messages');
 
 const {
   userJoin,
   getCurrentUser,
   userLeave,
   getRoomUsers,
-} = require("./utils/users");
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = "ChatCord Bot";
+const botName = 'ChatCord Bot';
 
 // Run when client connects
-io.on("connection", (socket) => {
-  socket.on("joinRoom", ({ username, room }) => {
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
 
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
     // Broadcast when a user connects
     socket.broadcast
       .to(user.room)
       .emit(
-        "message",
+        'message',
         formatMessage(botName, `${user.username} has joined the chat`)
       );
 
     // Send users and room info
-    io.to(user.room).emit("roomUsers", {
+    io.to(user.room).emit('roomUsers', {
       room: user.room,
       users: getRoomUsers(user.room),
     });
   });
 
   // Listen for chatMessage
-  socket.on("chatMessage", (msg) => {
+  socket.on('chatMessage', (msg) => {
     const user = getCurrentUser(socket.id);
 
-    io.to(user.room).emit("message", formatMessage(user.username, msg));
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
   });
 
   // Runs when client disconnects
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     const user = userLeave(socket.id);
 
     if (user) {
       io.to(user.room).emit(
-        "message",
+        'message',
         formatMessage(botName, `${user.username} has left the chat`)
       );
 
       // Send users and room info
-      io.to(user.room).emit("roomUsers", {
+      io.to(user.room).emit('roomUsers', {
         room: user.room,
         users: getRoomUsers(user.room),
       });
@@ -77,4 +78,13 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    server.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
